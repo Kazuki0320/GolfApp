85924de54f43b131e5bf1359a7da034f7506ed97
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2022-11-22 22:56:52 +0900

    vuetify/vuerouter

--- vue-golf/src/App.vue
+++ vue-golf/src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <v-navigation-drawer
+		v-model="drawer"
+		app
+	>
+		<v-sheet
+		color="grey lighten-4"
+		class="pa-4"
+		>
+		<v-avatar
+			class="mb-4"
+			color="grey darken-1"
+			size="64"
+		></v-avatar>
+
+		<div class="username">test</div>
+		</v-sheet>
+
+		<v-divider></v-divider>
+
+		<v-list>
+		<v-list-item
+			v-for="[icon, text, to] in links"
+			:key="icon"
+      :to="to"
+			link
+		>
+			<v-list-item-icon>
+			<v-icon>{{ icon }}</v-icon>
+			</v-list-item-icon>
+
+			<v-list-item-content>
+			<v-list-item-title>{{ text }}</v-list-item-title>
+			</v-list-item-content>
+		</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+.grey.darken-1 {
+  background-color: #757575 !important;
+  border-color: #757575 !important;
+}
+
+.pa-4 {
+  padding: 16px !important;
+}
+
+.username {
+  padding-top: 10px;
 }
 </style>
+
+<script>
+	export default {
+	data: () => ({
+		drawer: null,
+		links: [
+		['mdi-inbox-arrow-down', 'Inbox','/'],
+		['mdi-send', 'Send','/about'],
+		['mdi-delete', 'Trash','/about'],
+		['mdi-alert-octagon', 'Spam','/about'],
+		],
+	}),
+	}
+</script>
