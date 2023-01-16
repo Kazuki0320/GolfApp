50225d7e98251fa77e2856b6d525af649987c008
ittyi <vioiv.k4@gmail.com>
2023-01-16 21:50:16 +0900

    ✨ eslintをNodeに対応

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:vue/essential",
-        "eslint:recommended",
-        "prettier"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        'vue/multi-word-component-names': 'off',
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
 };
