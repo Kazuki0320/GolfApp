2f392dcdc4cf4af8089cf50e5992f23e7481054e
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2023-01-19 18:56:07 +0900

    feat:roomsの中にドキュメントIDとmessageを追加するための処理追加

--- src/views/SurveyConfirmed.vue
+++ src/views/SurveyConfirmed.vue
@@ -210,6 +210,8 @@ import firebase from "@/firebase/firebase"
 
 export default {
 	async created() {
+		this.user_id = this.$route.query.user_id;
+
 		// //車の有無を表示するための処理
 		this.AvailabilityOfCar = (this.carsModel ? '有' : '無')
 		// //スルーorランチ付きかを判断する処理
@@ -223,8 +225,7 @@ export default {
 		})
 	},
 	data: () => ({
-		user: '',
-		users:[],
+		userData: '',
 		user_id: '',
 		schedules: '',
 		friendsArray: [],
@@ -236,34 +237,58 @@ export default {
 		active: true,
 		answered: false,
 		friendsIdArray: [],
+		rooms: [],
 	}),
 	methods: {
 		async onClick() {
-			const questionnairesRef = firebase.firestore().collection("questionnaires")
-				const result1 = await questionnairesRef.add({
-					active: this.active,
-					answered: this.answered,
-					room_id: this.room_id,
-					users_id: JSON.stringify(this.friendsIdArray),
-					DeadlineForResponse: this.deadLineDate,
-				})
-				this.questionnairesId = result1.id
+			const userRef = firebase.firestore().collection("users").doc(this.user_id)
+			const userGet = await userRef.get()
+			this.userData = userGet.data()
 
-			const schedulesRef = firebase.firestore().collection("schedules")
-				await schedulesRef.add({
-				questionnairesId: this.questionnairesId,
-				member: JSON.stringify(this.friendsIdArray),
-				price: this.priceModel,
-				playTime: this.playTimeModel,
-				selectPlace1: this.candidatePrefectureModel1,
-				selectPlace2: this.candidatePrefectureModel2,
-				proposedDate: this.date,
-				AvailabilityOfCar: this.carsModel,
-				throughOrLunch: this.lunchModel,
-				AvailabilityOfCaddy: this.caddyModel,
-				remarks: this.remarkModel,
+			//roomsのドキュメントIDを作成
+			//フィールドの値も追加
+			const roomRef = await firebase.firestore().collection("rooms").add({
+				user_id: this.user_id,
+				name: this.userData.userName
 			})
-			this.$router.push('/')
+			// console.log("roomRef", roomRef)
+
+			//roomsのドキュメントIDの中にサブコレクションmessageを追加
+			//一旦、追加したフィールドにuser_idを保存。←挙動確認のために入れた適当な値
+			const questionnairesRef = firebase.firestore().collection("rooms").doc(roomRef.id)
+			const questionnairesAdd = await questionnairesRef.collection("massage").add({
+				user_id: this.user_id,
+			})
+			// console.log("questionnairesRef", questionnairesAdd)
+				// const result1 = await questionnairesRef.add({
+				// 	active: this.active,
+				// 	answered: this.answered,
+				// 	room_id: this.room_id,
+				// 	users_id: JSON.stringify(this.friendsIdArray),
+				// 	DeadlineForResponse: this.deadLineDate,
+				// 	message: this.body,
+				// 	makeUser: this.userData.userName,
+				// 	//後から、photoURLは追加すると思うから、一旦残し
+				// 	// photoURL: this.auth.photoURL,
+				// 	createdAt: firebase.firestore.Timestamp.now()
+				// })
+				// this.questionnairesId = result1.id
+
+			// const schedulesRef = firebase.firestore().collection("schedules")
+			// 	await schedulesRef.add({
+			// 	questionnairesId: this.questionnairesId,
+			// 	member: JSON.stringify(this.friendsIdArray),
+			// 	price: this.priceModel,
+			// 	playTime: this.playTimeModel,
+			// 	selectPlace1: this.candidatePrefectureModel1,
+			// 	selectPlace2: this.candidatePrefectureModel2,
+			// 	proposedDate: this.date,
+			// 	AvailabilityOfCar: this.carsModel,
+			// 	throughOrLunch: this.lunchModel,
+			// 	AvailabilityOfCaddy: this.caddyModel,
+			// 	remarks: this.remarkModel,
+			// })
+			// this.$router.push('/')
 		},
 	},
 	computed: {
